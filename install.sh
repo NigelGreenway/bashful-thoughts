293ed47d14aa537ce85c3612c03cf3491481fba1
Nigel Greenway <github@futurepixels.co.uk>
2017-03-27 21:26:49 +0100

    [a] create instal script for utility

--- install.sh
+++ install.sh
@@ -2,8 +2,13 @@
 
 location="$(readlink -f `pwd`)"
 
-mkdir -p $HOME/.config/bashful-thoughts
-cp $location/src/bashful_thoughts.conf $HOME/.config/bashful-thoughts/.bashful
+sudo mkdir -p $HOME/.config/bashful-thoughts
+sudo cp $location/src/bashful_thoughts.conf $HOME/.config/bashful-thoughts/.bashful
+sudo cp $location/src/bashful-thoughts.utility.bash /usr/local/bin/bashful
+
+sudo chmod +x /usr/local/bin/bashful
 
 
-echo -e "Please add the following to your shell profile\n\nsource $location/src/bashful-thoughts/utility.bash"
+echo -e "Please add the following to your shell profile\n"
+echo -e "The following will stop your thoughts being saved in your history. You will neen to log out for the full changes to take effect\n\n"
+echo -e "export HISTIGNORE=\"\$HISTIGNORE:bashful*\""
